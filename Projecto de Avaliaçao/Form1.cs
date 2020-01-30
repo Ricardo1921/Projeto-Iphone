f27995b24afd279121929b371f2807a027ab3fa8
Ricardo1921 <49916582+Ricardo1921@users.noreply.github.com>
2020-01-30 11:43:12 +0000

    Add files via upload

---
+++ Projecto de Avaliaçao/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projecto_de_Avaliaçao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        iphone telemovel = new iphone();
+        private void click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clicar(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void B1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+        }
+
+        private void B2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+        }
+
+        private void B3_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Visible = true;
+        }
+
+        private void B4_Click(object sender, EventArgs e)
+        {
+            pictureBox4.Visible = true;
+        }
+
+        private void B5_Click(object sender, EventArgs e)
+        {
+            pictureBox5.Visible = true;
+        }
+
+        private void B6_Click(object sender, EventArgs e)
+        {
+            pictureBox6.Visible = true;
+        }
+
+        private void B7_Click(object sender, EventArgs e)
+        {
+            pictureBox7.Visible = true;
+        }
+
+        private void B8_Click(object sender, EventArgs e)
+        {
+            pictureBox8.Visible = true;
+        }
+
+        private void B9_Click(object sender, EventArgs e)
+        {
+            pictureBox9.Visible = true;
+        }
+
+        private void B24_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
+            pictureBox7.Visible = false;
+            pictureBox8.Visible = false;
+            pictureBox9.Visible = false;
+            pictureBox10.Visible = false;
+            pictureBox11.Visible = false;
+            pictureBox12.Visible = false;
+            pictureBox13.Visible = false;
+            pictureBox14.Visible = false;
+            pictureBox15.Visible = false;
+            pictureBox16.Visible = false;
+            pictureBox17.Visible = false;
+            pictureBox18.Visible = false;
+            pictureBox19.Visible = false;
+            pictureBox20.Visible = false;
+            pictureBox21.Visible = false;
+            pictureBox22.Visible = false;
+            pictureBox23.Visible = false;
+        }
+
+        private void B10_Click(object sender, EventArgs e)
+        {
+            pictureBox10.Visible = true;
+        }
+
+        private void B11_Click(object sender, EventArgs e)
+        {
+            pictureBox11.Visible = true;
+        }
+
+        private void B12_Click(object sender, EventArgs e)
+        {
+            pictureBox12.Visible = true;
+        }
+
+        private void B13_Click(object sender, EventArgs e)
+        {
+            pictureBox13.Visible = true;
+        }
+
+        private void B14_Click(object sender, EventArgs e)
+        {
+            pictureBox14.Visible = true;
+        }
+
+        private void B15_Click(object sender, EventArgs e)
+        {
+            pictureBox15.Visible = true;
+        }
+
+        private void B16_Click(object sender, EventArgs e)
+        {
+            pictureBox16.Visible = true;
+        }
+
+        private void B17_Click(object sender, EventArgs e)
+        {
+            pictureBox17.Visible = true;
+        }
+
+        private void B18_Click(object sender, EventArgs e)
+        {
+            pictureBox18.Visible = true;
+        }
+
+        private void B19_Click(object sender, EventArgs e)
+        {
+            pictureBox19.Visible = true;
+        }
+
+        private void B20_Click(object sender, EventArgs e)
+        {
+            pictureBox20.Visible = true;
+        }
+
+        private void B21_Click(object sender, EventArgs e)
+        {
+            pictureBox21.Visible = true;
+        }
+
+        private void B22_Click(object sender, EventArgs e)
+        {
+            pictureBox22.Visible = true;
+        }
+
+        private void PictureBox21_Click(object sender, EventArgs e)
+        {
+            pictureBox21.Visible = true;
+        }
+
+        private void B23_Click(object sender, EventArgs e)
+        {
+            pictureBox23.Visible = true;
+        }
+    }
+}
---
+++ Projecto de Avaliaçao/iphode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecto_de_Avaliaçao
+{
+    class iphode
+    {
+    }
+}
